09c076eb5ca3130a7311530f6577aee2b63dcb8f
SyedMuneebHussain <muneebhussain1224@gmail.com>
2024-11-14 06:51:24 +0000

    container config done

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,7 +1,31 @@
-const name="muneeb";
-const repocount=50;
-console.log(`my name is ${name} and repocount is ${repocount}`);
-let Name=new String('muneeb');
-console.log(Name);
-console.log(typeof Name);
+// const name="muneeb";
+// const repocount=50;
+// console.log(`my name is ${name} and repocount is ${repocount}`);
+// let Name=new String('muneeb');
+// console.log(Name);
+// console.log(typeof Name);
+// console.log(Name[0]);
+// console.log(Name.length);
+// console.log(Name.toUpperCase());
+// console.log(Name.charAt(3));
+// console.log(Name.indexOf("e"));
+// let newsname=Name.substring(1,4);
+// console.log(newsname);
+
+
+let newName="   muneeb   ";
+console.log(newName);
+console.log(newName.trim());//trim method remove spaces from strings
+let url="https//muneeb.com/muneeb%20rfhj";
+console.log(url.replace("%20","_")); //it replace the value with another value
+console.log(url.includes("munee"));//this is use for search value in string
+let lastName="munneeb_shah_com";
+console.log(lastName.split("_"));//split use to seperate string and convert into array
+
+
+
+
+
+
+
 
