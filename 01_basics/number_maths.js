bb81757f489babe44b5a2f1051eef81b3783978e
SyedMuneebHussain <muneebhussain1224@gmail.com>
2024-11-14 07:47:40 +0000

    container config done

---
+++ 01_basics/number_maths.js
@@ -0,0 +1,31 @@
+// const score=100;
+// const balance=new Number(200);
+// //console.log(score);
+// //console.log(balance);
+// console.log(balance.toString().length);//it convert it into string and give us length 
+
+// console.log(balance.toFixed(1));//it fixed the value after . 
+// const newNumber=123.8966;
+// console.log(newNumber.toPrecision(4));// it precise the value how much we want and round of
+
+// const numbers=100000;
+// console.log(numbers.toLocaleString());// its convert value in 100,000
+
+
+//***************Maths************** */
+
+// console.log(Math.abs(-4));//it convert negative sign in positive
+// console.log(Math.round(4.5));//it round off decimal value
+// console.log(Math.ceil(4.2));//it will always round of in maximum value 4.2 roundoff 5
+// console.log(Math.floor(4.8));//it will always round of in minimum value 4.8 in 4
+// console.log(Math.min(4,6,2,8));
+// console.log(Math.max(4,6,2,8));
+console.log(Math.random()+1);
+console.log(Math.floor((Math.random()*10)+1));
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
